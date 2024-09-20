15314d2667e032718f46e58b385c811a9df682fd
Aakash_074 <aakash@getsafle.com>
2024-09-20 18:51:37 +0800

    fix: wallet connect + hedera testnet

---
+++ src/UploadComponent.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { UploadOutlined } from '@ant-design/icons';
+import type { UploadProps } from 'antd';
+import { Button, message, Upload } from 'antd';
+import axios from 'axios'; // You will need axios to handle the file upload manually
+
+const JWT = import.meta.env.VITE_PINATA_JWT;
+// Replace with your Pinata API key and secret
+const PINATA_API_KEY = import.meta.env.VITE_PINATA_API_KEY;
+const PINATA_SECRET_KEY = import.meta.env.VITE_PINATA_SECRET_KEY;
+
+const uploadToPinata = async (file: File) => {
+  const url = 'https://api.pinata.cloud/pinning/pinFileToIPFS';
+  
+  const formData = new FormData();
+  formData.append('file', file);
+
+  try {
+    const response = await axios.post(url, formData, {
+      headers: {
+        'Authorization': `Bearer ${JWT}`,
+        'pinata_api_key': PINATA_API_KEY,
+        'pinata_secret_api_key': PINATA_SECRET_KEY,
+      },
+    });
+    return response.data; // Adjust this based on the response structure
+  } catch (error) {
+    console.error('Error uploading file to Pinata:', error);
+    throw error;
+  }
+};
+
+const App: React.FC = () => { 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const handleChange = async (info: any) => {
+    if (info.file.status !== 'uploading') {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      try {
+        const result = await uploadToPinata(info.file.originFileObj);
+        message.success(`File uploaded successfully: ${result.IpfsHash}`);
+      } catch (error) {
+        console.log(error)
+        message.error('File upload failed.');
+      }
+    } else if (info.file.status === 'error') {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  };
+
+  const props: UploadProps = {
+    customRequest: async ({ file, onSuccess, onError }) => {
+      try { //@ts-expect-error any
+        await uploadToPinata(file);
+        onSuccess?.(file);
+      } catch (error) {
+        console.log(error)
+        //@ts-expect-error any
+        onError?.(error);
+      }
+    },
+    onChange: handleChange,
+  };
+
+  return (
+    <Upload {...props}>
+      <Button icon={<UploadOutlined />}>Click to Upload</Button>
+    </Upload>
+  );
+};
+
+export default App;
---
+++ src/Web3ModalProvider.tsx
@@ -0,0 +1,47 @@
+
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+
+import { WagmiProvider } from 'wagmi'
+import { hedera, hederaTestnet, mainnet } from 'wagmi/chains'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+// 0. Setup queryClient
+const queryClient = new QueryClient()
+
+// 1. Your Reown Cloud project ID
+const projectId = import.meta.env.VITE_REOWN_PROJECT_ID
+console.log(projectId)
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'bucketreel',
+  description: 'AppKit Example',
+  url: 'https://reown.com/appkit', // origin must match your domain & subdomain
+  icons: ['https://assets.reown.com/reown-profile-pic.png']
+}
+
+const chains = [mainnet, hedera, hederaTestnet] as const
+const config = defaultWagmiConfig({
+  chains,
+  projectId,
+  metadata,
+})
+
+// 3. Create modal
+createWeb3Modal({
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true, // Optional - defaults to your Cloud configuration
+  enableOnramp: true // Optional - false as default
+})
+
+//@ts-expect-error any
+export default function Web3ModalProvider({ children }) {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+    
