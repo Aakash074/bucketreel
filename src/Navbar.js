e5ce456ae3cc643a1079e030927b8c55cc53b064
Aakash_074 <aakash@getsafle.com>
2024-09-21 13:51:33 +0800

    fix: ui-corrections

--- src/Navbar.js
+++
@@ -1,6 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { Button } from 'antd';
-const Navbar = () => {
-    return (_jsxs("div", { className: "navbar", children: [_jsx("img", { src: "/path/to/logo.png", alt: "Logo", className: "logo" }), _jsx(Button, { type: "primary", children: "Button" })] }));
-};
-export default Navbar;
--- src/displayNFTs.tsx
+++ src/displayNFTs.tsx
@@ -34,25 +34,27 @@ const NFTDisplay = () => {
   return (
     <div className='p-4'>
       <h2 className='text-md font-bold'>Minted Location NFTs</h2>
-      <div className="nft-list flex flex-wrap">
+      <div className="nft-list flex justify-center flex-wrap">
       {/* @ts-ignore */}
         {nfts.filter(nft => nft.metadata && nft.metadata?.attributes?.[0]?.trait_type).map(nft => (
             // @ts-ignore
-          <Card key={nft.tokenInfo?.token_id} className="nft-card flex justify-center items-center m-2">
+          <Card key={nft.tokenInfo?.token_id} className="nft-card flex justify-center items-center m-2 bg-[#f2f2f27d] w-[320px]">
             {/* @ts-ignore */}
-            <img src={`https://peach-accused-eel-595.mypinata.cloud/ipfs/${nft.metadata?.image?.split('ipfs://')[1]}`} alt={nft.metadata?.name} className='rounded-md' style={{ width: '100%', maxWidth: '250px' }} />
-            <div className='flex flex-row justify-between items-center pt-4'>
-            <div className='flex flex-col'>
+            <img src={`https://peach-accused-eel-595.mypinata.cloud/ipfs/${nft.metadata?.image?.split('ipfs://')[1]}`} alt={nft.metadata?.name} className='rounded-md' style={{ width: '100%' }} />
+            <div className='flex flex-row justify-between items-center pt-4 w-full gap-2'>
+            <div className='flex flex-col w-[60%]'>
             {/* @ts-ignore */}
                 <h3>{nft.metadata?.name}</h3>
                 {/* @ts-ignore */}
                 <p>{nft.metadata?.description}</p>
             </div>
+            <div className='w-[40%] flex flex-row-reverse'>
             {/* @ts-ignore */}
             <Button onClick={() => handleLike(nft.tokenInfo?.token_id)}>
             {/* @ts-ignore */}
               Like ({likeCounts[nft?.tokenInfo?.token_id] || 0})
             </Button>
+            </div>
             </div>
           </Card>
         ))}
